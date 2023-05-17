f3a91bd550925d590079581b69929c23d77599a4
Johnny Ngo <johnnyhngo@yahoo.com>
2023-05-16 22:22:12 -0400

    @Emotion Styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from './logo.svg';
 import PropTypes from "prop-types";
 import './App.css';
+import styled from "@emotion/styled";
 
 
 
@@ -57,35 +57,46 @@ PokemonRow.propTypes = {
   onSelect: PropTypes.arrayOf(PropTypes.string.isRequired),
 }
 
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+const WholePage = styled.div`
+  margin: auto; 
+  width: 800px;
+  padding-top: 1rem;
+`;
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
+
+
 function App() {
   const [filter, filterSet] = React.useState("");
   const [selectedItem, selectedItemSet] = React.useState(null);
   const [pokemon, pokemonSet] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("http://localhost:3000/React-Practice/pokemon.json")
+    fetch("http://localhost:3004/React-Practice/pokemon.json")
     .then(resp => resp.json())
     .then(data => pokemonSet(data));
   }, [])
 
 
   return (
-    <div
-      style={{
-        margin: "auto", 
-        width: 800,
-        paddingTop: "1rem",
-      }}
-    >
-      <h1 className="title">Pokemon Search</h1>
-      <input value= {filter} onChange= {(evt) => filterSet(evt.target.value)}/>
-      <div
-        style = {{
-          display: 'grid',
-          gridTemplateColumns: "70% 30%",
-          gridColumnGap: "1rem",
-        }}
-      >
+    <WholePage>
+      <Title>Pokemon Search</Title>
+      <Input value= {filter} onChange= {(evt) => filterSet(evt.target.value)}/>
+      <TwoColumnLayout>
         <div>
         <table width="100%">
           <thead>
@@ -104,8 +115,8 @@ function App() {
         </table>
       </div>
       {selectedItem && <PokemonInfo {...selectedItem} />}
-     </div>
-    </div>
+     </TwoColumnLayout>
+    </WholePage>
     );
 
 }
